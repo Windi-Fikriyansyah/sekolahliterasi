00b968cdaf2559b978f19652cc916719d6e7a0be
windi fikriyansyah <diwin321@gmail.com>
2025-11-07 16:32:40 +0700

    da

--- resources/views/lp_mitra/viewer.blade.php
+++ resources/views/lp_mitra/viewer.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Landing PDF</title>
+    <title>{{ $mitra->judul }}</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.14.305/pdf.min.js"></script>
 
     <style>
